fd3229aedb0dc501d1cb6b2a27160cffe281de1e
Utshav Khatiwada <khatiwuv@18sh704md06.ocas.uc.edu>
2024-08-19 17:03:04 -0400

    Basic newassessment from

--- packages/client/src/pages/Assessments/NewAssessment.jsx
+++ packages/client/src/pages/Assessments/NewAssessment.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 import { AssessmentService } from '../../services/AssessmentService';
-
 export const NewAssessment = () => {
+
+  const { handleSubmit, register } = useForm();
 
   // create a form that utilizes the "onSubmit" function to send data to
   // packages/client/src/services/AssessmentService.js and then onto the packages/api/src/routes/assessment express API
@@ -10,7 +13,21 @@ export const NewAssessment = () => {
     await AssessmentService.submit(data);
   };
 
-  return <Form>
-    <Button variant="primary" type="submit">Submit</Button>
-  </Form>;
+  return (
+    <Form>
+      <label htmlFor="catName">Cat Name</label>
+      <input placeholder="Cat Name" />
+      <label htmlFor="CatDob">Cat Date of birth</label>
+      <input placeholder="Cat Date of birth" />
+      <br />
+      <label htmlFor="prevContact">Previous contact with the Cat Judical System</label>
+      <br />
+      <input type="radio" id="yes" />
+      <label htmlFor="yes">Yes</label>
+      <input type="radio" id="No" />
+      <label htmlFor="No">No</label>
+      <Button variant="primary" type="submit">Submit</Button>
+    </Form>
+  );
+
 };
